5b52b56dd9fb244a0e344bda220ad2d5da147b69
Faith Rider <fmhrider@gmail.com>
2022-10-11 14:06:30 -0400

    added color

--- main.py
+++ main.py
@@ -109,29 +109,30 @@ Section Two: Creating and placing the GUI.
 # Creates GUI window and title.
 window = Tk()
 window.title("Stage One: Setting Grid Values")
+window.configure(bg='#C7F9CC')
 
 
-row0_col0 = Button(window, text="0", padx=40, pady=20, command=lambda: user_click_grid(row0_col0))
-row0_col1 = Button(window, text="0", padx=40, pady=20, command=lambda: user_click_grid(row0_col1))
-row0_col2 = Button(window, text="0", padx=40, pady=20, command=lambda: user_click_grid(row0_col2))
-row0_col3 = Button(window, text="0", padx=40, pady=20, command=lambda: user_click_grid(row0_col3))
+row0_col0 = Button(window, text="0", bg='#57CC99', fg='#C7F9CC', padx=40, pady=20, command=lambda: user_click_grid(row0_col0))
+row0_col1 = Button(window, text="0", bg='#57CC99', fg='#C7F9CC', padx=40, pady=20, command=lambda: user_click_grid(row0_col1))
+row0_col2 = Button(window, text="0", bg='#57CC99', fg='#C7F9CC', padx=40, pady=20, command=lambda: user_click_grid(row0_col2))
+row0_col3 = Button(window, text="0", bg='#57CC99', fg='#C7F9CC', padx=40, pady=20, command=lambda: user_click_grid(row0_col3))
 
-row1_col0 = Button(window, text="0", padx=40, pady=20, command=lambda: user_click_grid(row1_col0))
-row1_col1 = Button(window, text="0", padx=40, pady=20, command=lambda: user_click_grid(row1_col1))
-row1_col2 = Button(window, text="0", padx=40, pady=20, command=lambda: user_click_grid(row1_col2))
-row1_col3 = Button(window, text="0", padx=40, pady=20, command=lambda: user_click_grid(row1_col3))
+row1_col0 = Button(window, text="0", bg='#57CC99', fg='#C7F9CC', padx=40, pady=20, command=lambda: user_click_grid(row1_col0))
+row1_col1 = Button(window, text="0", bg='#57CC99', fg='#C7F9CC', padx=40, pady=20, command=lambda: user_click_grid(row1_col1))
+row1_col2 = Button(window, text="0", bg='#57CC99', fg='#C7F9CC', padx=40, pady=20, command=lambda: user_click_grid(row1_col2))
+row1_col3 = Button(window, text="0", bg='#57CC99', fg='#C7F9CC', padx=40, pady=20, command=lambda: user_click_grid(row1_col3))
 
-row2_col0 = Button(window, text="0", padx=40, pady=20, command=lambda: user_click_grid(row2_col0))
-row2_col1 = Button(window, text="0", padx=40, pady=20, command=lambda: user_click_grid(row2_col1))
-row2_col2 = Button(window, text="0", padx=40, pady=20, command=lambda: user_click_grid(row2_col2))
-row2_col3 = Button(window, text="0", padx=40, pady=20, command=lambda: user_click_grid(row2_col3))
+row2_col0 = Button(window, text="0", bg='#57CC99', fg='#C7F9CC', padx=40, pady=20, command=lambda: user_click_grid(row2_col0))
+row2_col1 = Button(window, text="0", bg='#57CC99', fg='#C7F9CC', padx=40, pady=20, command=lambda: user_click_grid(row2_col1))
+row2_col2 = Button(window, text="0", bg='#57CC99', fg='#C7F9CC', padx=40, pady=20, command=lambda: user_click_grid(row2_col2))
+row2_col3 = Button(window, text="0", bg='#57CC99', fg='#C7F9CC', padx=40, pady=20, command=lambda: user_click_grid(row2_col3))
 
-row3_col0 = Button(window, text="0", padx=40, pady=20, command=lambda: user_click_grid(row3_col0))
-row3_col1 = Button(window, text="0", padx=40, pady=20, command=lambda: user_click_grid(row3_col1))
-row3_col2 = Button(window, text="0", padx=40, pady=20, command=lambda: user_click_grid(row3_col2))
-row3_col3 = Button(window, text="0", padx=40, pady=20, command=lambda: user_click_grid(row3_col3))
+row3_col0 = Button(window, text="0", bg='#57CC99', fg='#C7F9CC', padx=40, pady=20, command=lambda: user_click_grid(row3_col0))
+row3_col1 = Button(window, text="0", bg='#57CC99', fg='#C7F9CC', padx=40, pady=20, command=lambda: user_click_grid(row3_col1))
+row3_col2 = Button(window, text="0", bg='#57CC99', fg='#C7F9CC', padx=40, pady=20, command=lambda: user_click_grid(row3_col2))
+row3_col3 = Button(window, text="0", bg='#57CC99', fg='#C7F9CC', padx=40, pady=20, command=lambda: user_click_grid(row3_col3))
 
-instructions = Label(window, text = "Press the bottom buttons or type to enter a value, the press the corresponding table cell to change it! When you're ready to interpolate, press the \"Ready to Interpolate\" button.")
+instructions = Label(window, bg='#C7F9CC', fg='#22577A', text = "Press the bottom buttons or type to enter a value,\npress the corresponding table cell to change it!\n\nWhen you're ready to interpolate,\npress the \"Ready to Interpolate\" button.")
 
 
 row0_col0.grid(row=0, column=0)
@@ -158,26 +159,26 @@ instructions.grid(row=4,column=0, columnspan=10)
 
 
 # Creates the entry/showcase box.
-entry = Entry(window, width=35, borderwidth=5)
-entry.grid(row=5,column=0, columnspan=10, padx=10, pady=10)
+entry = Entry(window, bg='#C7F9CC', width=35, borderwidth=5)
+entry.grid(row=5,column=0, columnspan=4, padx=10, pady=10)
 
 # 0-9 Buttons for the user to input values, the clear button, and the button to change to interpolation mode.
-button_0 = Button(window, text="0", padx=40, pady=20, command=lambda: user_click(0))
+button_0 = Button(window, text="0", bg='#38A3A5', fg='#C7F9CC', padx=40, pady=20, command=lambda: user_click(0))
 
-button_1 = Button(window, text="1", padx=40, pady=20, command=lambda: user_click(1))
-button_2 = Button(window, text="2", padx=40, pady=20, command=lambda: user_click(2))
-button_3 = Button(window, text="3", padx=40, pady=20, command=lambda: user_click(3))
+button_1 = Button(window, text="1", bg='#38A3A5', fg='#C7F9CC',padx=40, pady=20, command=lambda: user_click(1))
+button_2 = Button(window, text="2", bg='#38A3A5', fg='#C7F9CC',padx=40, pady=20, command=lambda: user_click(2))
+button_3 = Button(window, text="3", bg='#38A3A5', fg='#C7F9CC',padx=40, pady=20, command=lambda: user_click(3))
 
-button_4 = Button(window, text="4", padx=40, pady=20, command=lambda: user_click(4))
-button_5 = Button(window, text="5", padx=40, pady=20, command=lambda: user_click(5))
-button_6 = Button(window, text="6", padx=40, pady=20, command=lambda: user_click(6))
+button_4 = Button(window, text="4", bg='#38A3A5', fg='#C7F9CC',padx=40, pady=20, command=lambda: user_click(4))
+button_5 = Button(window, text="5", bg='#38A3A5', fg='#C7F9CC',padx=40, pady=20, command=lambda: user_click(5))
+button_6 = Button(window, text="6", bg='#38A3A5', fg='#C7F9CC',padx=40, pady=20, command=lambda: user_click(6))
 
-button_7 = Button(window, text="7", padx=40, pady=20, command=lambda: user_click(7))
-button_8 = Button(window, text="8", padx=40, pady=20, command=lambda: user_click(8))
-button_9 = Button(window, text="9", padx=40, pady=20, command=lambda: user_click(9))
+button_7 = Button(window, text="7", bg='#38A3A5', fg='#C7F9CC',padx=40, pady=20, command=lambda: user_click(7))
+button_8 = Button(window, text="8", bg='#38A3A5', fg='#C7F9CC',padx=40, pady=20, command=lambda: user_click(8))
+button_9 = Button(window, text="9", bg='#38A3A5', fg='#C7F9CC',padx=40, pady=20, command=lambda: user_click(9))
 
-button_clear = Button(window, text="Clear Cell", padx=200, pady=20, command=user_click_clear)
-ready_to_interpolate = Button(window, text="Ready to Interpolate", padx=170, pady=20, command=switch_to_interpolation)
+button_clear = Button(window, text="Clear Cell", bg='#80ED99', fg='#C7F9CC', padx=160, pady=20, command=user_click_clear)
+ready_to_interpolate = Button(window, text="Ready to Interpolate", bg='#57CC99', fg='#C7F9CC', padx=132, pady=20, command=switch_to_interpolation)
 
 # Puts input buttons to window.
 button_0.grid(row=6, column=0)
@@ -186,16 +187,16 @@ button_1.grid(row=6, column=1)
 button_2.grid(row=6, column=2)
 button_3.grid(row=6, column=3)
 
-button_4.grid(row=6, column=4)
-button_5.grid(row=6, column=5)
-button_6.grid(row=6, column=6)
+button_4.grid(row=7, column=0)
+button_5.grid(row=7, column=1)
+button_6.grid(row=7, column=2)
 
-button_7.grid(row=6, column=7)
-button_8.grid(row=6, column=8)
-button_9.grid(row=6, column=9)
+button_7.grid(row=7, column=3)
+button_8.grid(row=8, column=1)
+button_9.grid(row=8, column=2)
 
-button_clear.grid(row=7, column=0, columnspan=5)
-ready_to_interpolate.grid(row=7, column=5, columnspan=5)
+button_clear.grid(row=9, column=0, columnspan=4)
+ready_to_interpolate.grid(row=10, column=0, columnspan=4)
 
 
 
